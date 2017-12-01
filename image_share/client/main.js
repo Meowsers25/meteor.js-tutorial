c41b4c7ceae7974baeede83b3e512eb436ef1192
Chris <c.lamothe25@gmail.com>
2017-12-01 18:08:01 -0500

    added bootstrap

--- image_share/client/main.js
+++ image_share/client/main.js
@@ -1,17 +1,26 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {
+  Template
+} from 'meteor/templating';
+import {
+  ReactiveVar
+} from 'meteor/reactive-var';
 
 import './main.html';
 
-var img_data = [
-{
-  img_src:"ND.jpeg",
-  img_alt:"Number Devil is the man!"
-},
-{
-  img_src:"IMG_0157.JPG",
-  img_alt:"LUNA!"
-},
+var img_data = [{
+    img_src: "ND.jpeg",
+    img_alt: "Number Devil is the man!"
+  },
+  {
+    img_src: "ND.jpeg",
+    img_alt: "Number Devil again"
+  },
+  {
+    img_src: "ND.jpeg",
+    img_alt: "Number Devil again"
+  }
 ];
 
-Template.images.helpers({images:img_data});
+Template.images.helpers({
+  images: img_data
+});
