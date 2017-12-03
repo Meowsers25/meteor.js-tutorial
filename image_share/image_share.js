daf4b2e8d1768bd82d0aa24e293ad3c4d23f8313
Chris <c.lamothe25@gmail.com>
2017-12-03 13:59:07 -0500

    added rating function with update capability

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -7,7 +7,7 @@ if (Meteor.isClient) {
   //Template.images.helpers({images:img_data});
 
   Template.images.helpers({
-    images: Images.find()
+    images: Images.find({}, {sort:{rating:-1}})
   });
 
 
@@ -21,6 +21,15 @@ if (Meteor.isClient) {
       $("#" + image_id).hide('slow', function() {
         Images.remove({"_id":image_id});
       })
-     }
+    },
+    'click .js-rate-image': function(event) {
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(rating);
+      var image_id = this.id;
+      console.log(image_id);
+
+      Images.update({_id:image_id},
+                    {$set: {rating:rating}});
+    }
   });
 }
