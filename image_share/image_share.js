62d58e2ae7f2b98f629b22289332a6ef48fdce22
Chris <c.lamothe25@gmail.com>
2017-12-05 13:41:25 -0500

    added username

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -2,13 +2,18 @@
 Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+  });
+
    Template.images.helpers({images:
     Images.find({}, {sort:{createdOn: -1, rating:-1}})
   });
 
   Template.body.helpers({username:function(){
     if (Meteor.user()) {
-      return Meteor.user().emails[0].address;
+      return Meteor.user().username;
+      //return Meteor.user().emails[0].address;
     }
     else {
       return "anonymous internet user";
